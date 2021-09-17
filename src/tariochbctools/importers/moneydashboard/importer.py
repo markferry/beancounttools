78271c505c9d92997bbf756add4e2a0487c508ba
Mark Ferry <mark@markferry.net>
2021-09-17 17:19:01 +0100

    moneydashboard: basic transaction import
    Usage: create a config 'moneydashboard.yaml' of the form
    
    ```yaml
    email: my.money@email.address
    password: mymoneypassword
    account: Liabilities:SomeAccount
    ```
    
    FIXME:
     * depends on a patch to moneydashboard to parse the date.
     * calls moneydashboard._get_transactions() to get the raw txs

--- src/tariochbctools/importers/moneydashboard/importer.py
+++ src/tariochbctools/importers/moneydashboard/importer.py
@@ -1,24 +1,90 @@
 from os import path
+from typing import List, NamedTuple, Optional
 
 import yaml
+from beancount.core import amount, data
+from beancount.core.number import D
 from beancount.ingest import importer
 from moneydashboard import MoneyDashboard
+from undictify import type_checked_constructor
+
+
+@type_checked_constructor(skip=True, convert=True)
+class MoneyDashboardTransaction(NamedTuple):
+    """Transaction data from MoneyDashboard transaction API"""
+
+    Id: str
+    Description: str
+    OriginalDescription: Optional[str]
+    Amount: str
+    Date: str
+    OriginalDate: str
+    IsDebit: bool
+    TagId: int
+    MerchantId: int
+    AccountId: int
+    Notes: Optional[str]
+    NativeCurrency: str
+    NativeAmount: str
+    CurrencyExchange: Optional[str]
+    AvailableCurrencyExchanges: Optional[List[str]]
+
+    def to_beancount_transaction(self, local_account, invert_sign=False):
+        tx_amount = D(self.NativeAmount)
+        # avoid pylint invalid-unary-operand-type
+        signed_amount = -1 * tx_amount if invert_sign else tx_amount
+
+        metakv = {
+            "mdref": self.Id,
+        }
+
+        if self.Notes:
+            metakv["notes"] = self.Notes
+
+        meta = data.new_metadata("", 0, metakv)
+        # FIXME: depends on a moneydashboard patch
+        date = MoneyDashboard.parse_wcf_date(self.Date).date()
+
+        entry = data.Transaction(
+            meta,
+            date,
+            "*",
+            "",
+            self.Description,
+            data.EMPTY_SET,
+            data.EMPTY_SET,
+            [
+                data.Posting(
+                    local_account,
+                    amount.Amount(signed_amount, self.NativeCurrency),
+                    None,
+                    None,
+                    None,
+                    None,
+                ),
+            ],
+        )
+        return entry
 
 
 class Importer(importer.ImporterProtocol):
-    """An importer for MoneyDashboard unofficial API"""
+    """An importer for MoneyDashboard"""
 
     def __init__(self):
         self.md = None
-        pass
+        self.account = None
+        self.existing_entries = None
 
     def _configure(self, file, existing_entries):
         with open(file.name, "r") as f:
             config = yaml.safe_load(f)
             self.md = MoneyDashboard(email=config["email"], password=config["password"])
 
+        self.existing_entries = existing_entries
+        self.account = config["account"]
+
     def identify(self, file):
-        return "moneydashboard.yaml" == path.basename(file.name)
+        return path.basename(file.name) == "moneydashboard.yaml"
 
     def file_account(self, file):
         return ""
@@ -26,5 +92,22 @@ class Importer(importer.ImporterProtocol):
     def extract(self, file, existing_entries=None):
         self._configure(file, existing_entries)
 
+        # FIXME: protected access
+        transactions = self.md._get_transactions(2)  # since last login
         entries = []
+
+        for trx in transactions:
+            entries.extend(
+                self._extract_transaction(trx, self.account, transactions, False)
+            )
+
+        return entries
+
+    def _extract_transaction(self, trx, local_account, transactions, invert_sign):
+        entries = []
+
+        t = MoneyDashboardTransaction(**trx)
+        entry = t.to_beancount_transaction(local_account, invert_sign)
+        entries.append(entry)
+
         return entries
--- tests/tariochbctools/importers/test_moneydashboard.py
+++ tests/tariochbctools/importers/test_moneydashboard.py
@@ -1,4 +1,7 @@
+import json
+
 import pytest
+from beancount.core import data
 from beancount.ingest import cache
 
 from tariochbctools.importers.moneydashboard import importer as mdimp
@@ -8,6 +11,27 @@ from tariochbctools.importers.moneydashboard import importer as mdimp
 TEST_CONFIG = b"""
     email: something@somewhere.com
     password: some-password
+    account: Some:Account
+"""
+
+TEST_TRX = """
+{
+    "Id": 754837318,
+    "Description": "WILLCROP LTD",
+    "OriginalDescription": "WILLCROP LTD",
+    "Amount": -15.35,
+    "Date": "/Date(1631664000000)/",
+    "OriginalDate": "/Date(-62135596800000)/",
+    "IsDebit": true,
+    "TagId": -1,
+    "MerchantId": 194632,
+    "AccountId": 1666071,
+    "Notes": null,
+    "NativeCurrency": "GBP",
+    "NativeAmount": -15.35,
+    "CurrencyExchange": null,
+    "AvailableCurrencyExchanges": []
+}
 """
 
 
@@ -19,11 +43,24 @@ def tmp_config_fixture(tmp_path):
 
 
 @pytest.fixture(name="importer")
-def moneydashboard_importer_fixture(tmp_config):
+def moneydashboard_importer_fixture(tmp_config, mocker):
+    mocker.patch("moneydashboard.MoneyDashboard._get_accounts", return_value=[])
     importer = mdimp.Importer()
     importer._configure(tmp_config, [])
     yield importer
 
 
+@pytest.fixture(name="tmp_trx")
+def tmp_trx_fixture():
+    yield json.loads(TEST_TRX)
+
+
 def test_identify(importer, tmp_config):
     assert importer.identify(tmp_config)
+
+
+def test_extract_transaction_simple(importer, tmp_trx):
+    entries = importer._extract_transaction(
+        tmp_trx, importer.account, [tmp_trx], invert_sign=False
+    )
+    data.sanity_check_types(entries[0])
